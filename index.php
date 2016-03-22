4c4f337034cc863ef91673d14c3f91e44f7eb14c
Teddy <r2d2f9@gmail.com>
2016-03-22 13:20:32 -0700

    Update index.php
    update index file from 97 to 134 lines

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en-ru">
 <head>
 	<meta charset="utf-8">
-	<title>Bible db</title>
+	<title>DiaryBible</title>
 	<meta name="copyright" Content="TeddyFrost2016">
 	<meta name=viewport content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
@@ -17,10 +17,11 @@
 				$query = "SELECT * FROM `u11111` ORDER BY `id` DESC LIMIT 0, 150";
 				$result = $con->query($query) or die($con->error.__LINE__);
 				mysqli_close($con);
+				/*
 				$result->fetch_assoc();
 				$a = 0;
 				$n = 0;
-				/*while ($line = $result->fetch_assoc()) {
+				while ($line = $result->fetch_assoc()) {
 					echo "<link>";
 					$n += $line['n'];
 					$a++;
@@ -30,6 +31,12 @@
 </head>
 <body>
 <header></header>
+<div id="infow">
+	<p class="infotext">Здесь есть информация</p>
+	<div class="infobuttons">
+		<div class="info-b-1">ОК</div>
+	</div>
+</div>
 <div id="background">
 	<img src="img/bg.jpg">
 </div>
@@ -37,22 +44,22 @@
 	<div id="profile">
 		<div id="image"><img src="img/image.png"></div>
 		<div id="name">Федя Мороз</div>
+		<input type="hidden" id="nameinp" value="u11111">
 	</div>
 	<div id="progress">
 		<div id="times">1</div>
-		<div id="progbar" val="<?php echo (100/1189) * $n; ?>"></div>
-		<div id="addprogr">+</div>
+		<div id="progbar"></div>
 	</div>
 	<div id="table">
 	<table  border="0">
 		<tr class="th">
-			<th style="width:10%;">Date</th>
-			<th>Chapters</th>
-			<th style="width:10%;">Number</th>
-			<th style="width:40%;">Description</th>
+			<th style="width:10%;">Дата</th>
+			<th>Главы</th>
+			<th style="width:10%;">Кол-во</th>
+			<th style="width:40%;">Описание</th>
 	   </tr>
 				<?php
-				$i = 0;
+				$i = 13;
 				$n = 0;
 				while ($row = $result->fetch_assoc()) {
 					echo '<tr num="'.$row['id'].'">';
@@ -63,32 +70,62 @@
 						echo '<td class="c" full="'.$row['c'].'">'.substr($row['c'], 0, 40).'</td>';
 					}
 					echo '<td class="n">'.$row['n'].'</td>';
-					if(strlen($row['s']) >= 50){
-						echo '<td class="s" full="'.$row['s'].'">'.substr($row['s'], 0, 40).'...</td>';
+					if(strlen($row['s']) >= 40){
+						echo '<td class="s" full="'.$row['s'].'">'.substr($row['s'], 0, 39).'...</td>';
 					}else{
-						echo '<td class="s" full="'.$row['s'].'">'.substr($row['s'], 0, 40).'</td>';
+						echo '<td class="s" full="'.$row['s'].'">'.substr($row['s'], 0, 39).'</td>';
 					}
 					echo '</tr>';
 					$n += $row['n'];
 					$i++;
 				}
-				echo '&nbsp Прочитано глав: '.$n;
-				echo '&nbsp Осталось глав: '. (1189 - $n);
-				echo '&nbsp В процентах: '.round((100/1189) * $n);
+				if($n > 1189){
+					$nt = $n - ( 1189 * floor($n / 1189));
+					//$NT = ceil($n)
+				echo '&nbsp Уровень: <span id="tlevel">'. (floor($n / 1189)+1).'</span>';
+				echo '&nbsp Прочитано глав всего : <span id="tallchap">'.$n.'</span>';
+				echo '&nbsp Прочитано глав за этот раз: <span id="tthischap">'.$nt.'</span>';
+				}else{
+					echo '&nbsp Прочитано глав: <span id="tallchap">'.$n.'</span>';
+					$nt = $n;
+				}
+				echo '&nbsp Осталось глав: <span id="tleftchap">'. (1189 - $nt).'</span>';
+				echo '&nbsp В процентах: <span id="tpercent">'.round((100/1189) * $nt,1).'</span>';
 	   ?>
 	</table></div> 
 </div>
-<div id="info" percent="<?php echo round((100/1189) * $n); ?>"></div>
+<div id="info" percent="
+<?php echo round((100/1189) * $nt,1); ?>" times="
+<?php echo floor($n / 1189)+1; ?>" date="
+<?php if(date('G')>4){$d = date('d');}else{ if(date('j')<10){$d = '0'.(date('j')-1);}else{$d = (date('j')-1);}}  echo $d.'.'.date('m').'.'.date('y'); ?>"></div>
 <div id="toppanel">
+	<div id="topback"><i class="fa  fa-angle-left fa-fw"></i>Назад</div>
+	<div id="topdelite"><i class="fa  fa-trash fa-fw"></i></div>
+	<div id="topadd"><i class="fa  fa-plus fa-fw"></i></div>
+	<div id="topedit"><i class="fa  fa-edit fa-fw"></i></div>
+	<div id="topsave" style="display:none"><i class="fa  fa-save fa-fw"></i></div>
 </div>
 <div id="win">
-	<input id="wdate" value="" readonly>
-	<input id="wnumber" value="" readonly>
+	<input type="hidden" id="wid">
+	<label id="wpass"  style="display:none">Пароль:
+		<input type="password" id="passinp">
+	</label><br>
+	<label id="wdatel">Дата:
+		<input id="wdate" value="" readonly>
+	</label>
+	<label id="wnuml">Количество:
+	<input type="number" min="0"  max="60" id="wnumber" value="" readonly>
+	</label>
+	<label id="wchl">Главы:
 	<input id="wchapters" value="" readonly>
+	</label>
+	<label id="wdescl">Описание:<br >
 	<textarea id="wdescription" readonly></textarea>
+	</label>
+	<div id="wspan"></div>
 </div>
 <footer>
-	<div style="color:#777;font-size:17px;">&copy;&nbsp;Bible db 2016</div><br>
+	<div style="color:#777;font-size:17px;">&copy;&nbsp;DiaryBible 2016</div><br>
 	<div style="color:#777;font-size:10px;">Made by TedFrost 2016</div>
 </footer>
 </body>
